2419b24064c2c3c16dda357b79987c31064a299f
Dinh Thanh Son <dinhthanhsondtu@gmail.com>
2020-11-25 11:52:27 +0700

    Add Entity

---
+++ ShopFashion.Data/EF/ShopFashionDbContext.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ShopFashion.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopFashion.Data.EF
+{
+    public class ShopFashionDbContext : DbContext
+    {
+        public ShopFashionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        public DbSet<Slide> Slides { get; set; }
+    }
+}
---
+++ ShopFashion.Data/Entities/Appconfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopFashion.Data.Entities
+{
+
+        public class AppConfig
+        {
+            public string Key { get; set; }
+
+            public string Value { get; set; }
+        }
+    
+}
